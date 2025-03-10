70ff8d82abe70445a95dd51f81fb4ad83830f9c0
Padalasai1009 <padalasai1009@gmail.com>
2025-03-10 20:03:12 +0530

    Update LaunchingFlipkart.java

--- LaunchingFlipkart.java
+++ LaunchingFlipkart.java
@@ -1,7 +1,4 @@
-package com.testNG;
-
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
