57841c8aa1e37218a446fc7d9152795d7a6c321a
Padalasai1009 <padalasai1009@gmail.com>
2025-03-10 20:02:40 +0530

    Add files via upload

---
+++ LaunchingFlipkart.java
@@ -0,0 +1,27 @@
+package com.testNG;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchingFlipkart {
+	WebDriver wbDriver;
+	
+	@BeforeTest
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		wbDriver = new ChromeDriver();
+		wbDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		wbDriver.manage().window().maximize();
+	}
+	
+	@Test
+	public void openingFlipkart() {
+		wbDriver.get("https://flipkart.com/");
+	}
+}
